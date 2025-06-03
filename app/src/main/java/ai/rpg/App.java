beaf1d5b3f393b54f5756ac861fb7a380902f0c5
Enrico <enrico@MacBookPro.box>
2025-06-03 15:47:36 +0200

    Update Gradle configuration for multi-module project

--- app/src/main/java/ai/rpg/App.java
+++ app/src/main/java/ai/rpg/App.java
@@ -3,12 +3,23 @@
  */
 package ai.rpg;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
     }
 }
+
+
